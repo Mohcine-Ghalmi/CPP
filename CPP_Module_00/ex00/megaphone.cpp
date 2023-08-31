07008abde331fd33aab0b2a1097e58154d9557e6
Mohcine Ghalmi <mghalmi@e3r4p4.1337.ma>
2023-08-31 06:46:52 +0100

    Readme

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -1,16 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 00:55:36 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/08/31 01:28:54 by mghalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string.h>
 
 int main(int argc, char **argv)
 {
     if (argc > 1)
-        for (int j = 1; j < argc; j++)
-            for (size_t i = 0; i < strlen(argv[j]); i++)
-            {
-                toupper(argv[j][i]);
-                std::cout << argv[j][i];
-            }
-    else
-        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        for (int i = 1; i < argc; i++)
+            for(size_t j = 0; j < strlen(argv[i]); j++)
+                std::cout << (char) toupper(argv[i][j]);
+    if (argc <= 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
     return (0);
 }
