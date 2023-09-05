09c9c5be234a53b1b3aed66f8566552b311dd743
Mohcine Ghalmi <mghalmi@e3r1p7.1337.ma>
2023-09-05 15:58:30 +0100

    ned to fix account

--- CPP_Module_00/ex02/Account.cpp
+++ CPP_Module_00/ex02/Account.cpp
@@ -6,29 +6,53 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 15:49:35 by mghalmi           #+#    #+#             */
-/*   Updated: 2023/09/03 16:18:25 by mghalmi          ###   ########.fr       */
+/*   Updated: 2023/09/05 15:38:29 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "Account.hpp"
+#include <iostream>
+#include <iomanip>
 
-int Account::getNbAccounts( void ) 
+int Account::getNbAccounts() 
 {
-    return _nbAccounts;
+	return _nbAccounts;
 }
 
-int Account::getTotalAmount( void ) 
+int Account::getTotalAmount() 
 {
-    return _totalAmount;
+	return _totalAmount;
 }
 
-int Account::getNbDeposits( void ) 
+int Account::getNbDeposits() 
 {
-    return _totalNbDeposits;
+	return _totalNbDeposits;
 }
 
-int Account::getNbWithdrawals( void ) 
+int Account::getNbWithdrawals() 
 {
-    return _totalNbWithdrawals;
+	return _totalNbWithdrawals;
+}
+
+void	Account::_displayTimestamp()
+{
+	std::time_t time;
+
+	time = std::time(NULL);
+	std::cout <<"[" << 1900 + std::localtime(&time)->tm_year;
+	std::cout << std::setw(2) << 1 + std::localtime(&time)->tm_mon;
+    std::cout << std::setw(2) << std::localtime(&time)->tm_mday;
+    std::cout << "_";
+    std::cout << std::setw(2) << std::localtime(&time)->tm_hour;
+    std::cout << std::setw(2) << std::localtime(&time)->tm_min;
+    std::cout << std::setw(2) << std::localtime(&time)->tm_sec ;
+    std::cout << "] " << std::flush;
+}
+
+void	Account::displayStatus() const
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";" << "amoutn:" << _amount << ";" 
+	<< "deposit:" << _nbDeposits << ";" << "withdrawls:" << _nbWithdrawals << std::endl;
 }
 
--- CPP_Module_00/ex02/Account.hpp
+++ CPP_Module_00/ex02/Account.hpp
@@ -19,24 +19,24 @@
 class Account {
     public:
         typedef Account		t;
-        static int	getNbAccounts( void );
-        static int	getTotalAmount( void );
-        static int	getNbDeposits( void );
-        static int	getNbWithdrawals( void );
+        static int	getNbAccounts( void );// done
+        static int	getTotalAmount( void );// done
+        static int	getNbDeposits( void );// done
+        static int	getNbWithdrawals( void );// done
         static void	displayAccountsInfos( void );
         Account( int initial_deposit );
         ~Account( void );
         void	makeDeposit( int deposit );
         bool	makeWithdrawal( int withdrawal );
         int		checkAmount( void ) const;
-        void	displayStatus( void ) const;
+        void	displayStatus( void ) const; // done
 
     private:
         static int	_nbAccounts;
         static int	_totalAmount;
         static int	_totalNbDeposits;
         static int	_totalNbWithdrawals;
-        static void	_displayTimestamp( void );
+        static void	_displayTimestamp( void );// done
         int				_accountIndex;
         int				_amount;
         int				_nbDeposits;
