c2d79a6738bcde4d59c54b85db875baf93d4a047
Mohcine Ghalmi <mghalmi@e3r5p3.1337.ma>
2023-09-15 17:53:42 +0100

    working in the ex00 module 1

---
+++ CPP_Module_01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 16:56:37 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/09/15 17:47:33 by mghalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+        void randomChump(std::string name);
+        Zombie* newZombie(std::string name);
+};
+
+#endif
