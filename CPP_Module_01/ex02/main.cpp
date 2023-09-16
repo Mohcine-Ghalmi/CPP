822fc6a98d4373a7f62f0637c3aaac0d237b5c3e
Mohcine Ghalmi <mghalmi@e3r7p15.1337.ma>
2023-09-16 22:54:11 +0100

    done with ex02 M01

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 20:35:47 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/09/16 22:41:57 by mghalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Memory address of the string variable: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of the string variable: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
