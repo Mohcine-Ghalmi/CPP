353922103de37394000c3b5ba6d572571b853e7e
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-09-27 15:16:59 +0100

    finished ex03 m02 in the main start with m03

--- CPP_Module_02/ex03/main.cpp
+++ CPP_Module_02/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sleeps <sleeps@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 21:04:51 by mghalmi           #+#    #+#             */
-/*   Updated: 2023/09/26 14:40:53 by mghalmi          ###   ########.fr       */
+/*   Updated: 2023/09/27 15:16:34 by sleeps           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 int main()
 {
-    if ( bsp( Point(0, 0), Point(10, 30), Point(20, 0), Point(30, 15) ) == true ) {
+    if (bsp( Point(0, 0), Point(10, 30), Point(20, 0), Point(30, 15) ))
         std::cout << "Point is in the triangle" << std::endl;
-    } else {
+    else
         std::cout << "Point is not in the triangle" << std::endl;
-    }
     return 0;
 }
